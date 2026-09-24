d91f29157564522f15623dd3b4cd24dbd74251b6
Icbeas5 <ishanchaurasia218@gmail.com>
2026-09-24 15:08:41 +0530

    dynamic

--- app/(admin)/admin/page.jsx
+++ app/(admin)/admin/page.jsx
@@ -2,6 +2,8 @@ import { getDashboardData } from "@/actions/admin";
 import Dashboard from "./_components/dashboard";
 import React from "react";
 
+export const dynamic = "force-dynamic";
+
 export const metadata = {
   title: "Dashboard | Vehiql Admin",
   description: "Admin dashboard for Vehiql car marketplace",
